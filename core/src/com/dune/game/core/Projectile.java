d97b3f4ac2128715ddc5e70659fc9045cb2fed3c
Ivan Petrov <petrovodt@gmail.com>
2020-05-27 20:58:56 +0300

    lesson-3 solution

--- core/src/com/dune/game/core/Projectile.java
+++ core/src/com/dune/game/core/Projectile.java
@@ -6,50 +6,44 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
 
-public class Projectile {
-    private Vector2 position;
-    private Vector2 velocity = new Vector2();
+public class Projectile extends GameObject implements Poolable {
     private TextureRegion texture;
-    private boolean projectileFlying = false;
+    private Vector2 velocity;
+    private float speed;
     private float angle;
+    private boolean active;
 
-    public Projectile(TextureAtlas atlas) {
-        this.texture = atlas.findRegion("bullet");
-        this.position = new Vector2(100.0f, 100.0f);
+    @Override
+    public boolean isActive() {
+        return active;
     }
 
-    public boolean isProjectileFlying() {return projectileFlying;}
-
-    public void checkBounds() {
-        if (position.x < 40) {
-            projectileFlying = false;
-        }
-        if (position.y < 40) {
-            projectileFlying = false;
-        }
-        if (position.x > 1240) {
-            projectileFlying = false;
-        }
-        if (position.y > 680) {
-            projectileFlying = false;
-        }
+    public void deactivate() {
+        active = false;
     }
 
-    public void shoot(Vector2 startPosition, float angle){
-        if(!projectileFlying){
-            projectileFlying = true;
-            this.angle = angle;
-            position.set(startPosition);
-            velocity.set(500.0f * MathUtils.cosDeg(angle), 500.0f * MathUtils.sinDeg(angle));
-        }
+    public Projectile(GameController gc) {
+        super(gc);
+        this.velocity = new Vector2();
+        this.speed = 320.0f;
     }
 
-    public void update(float dt){
-        checkBounds();
+    public void setup(Vector2 startPosition, float angle, TextureRegion texture) {
+        this.texture = texture;
+        this.position.set(startPosition);
+        this.angle = angle;
+        this.velocity.set(speed * MathUtils.cosDeg(angle), speed * MathUtils.sinDeg(angle));
+        this.active = true;
+    }
+
+    public void render(SpriteBatch batch) {
+        batch.draw(texture, position.x - 8, position.y - 8);
+    }
+
+    public void update(float dt) {
         position.mulAdd(velocity, dt);
-    }
-
-    public void draw(SpriteBatch batch) {
-        batch.draw(texture, position.x - 8, position.y - 8, 8, 8, 16, 16, 1, 1, angle);
+        if (position.x < 0 || position.x > 1280 || position.y < 0 || position.y > 720) {
+            deactivate();
+        }
     }
 }
