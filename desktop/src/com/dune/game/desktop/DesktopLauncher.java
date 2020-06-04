e7397c8129b93e5c709edb41b69d4cc9ac5c56c9
Ivan Petrov <petrovodt@gmail.com>
2020-06-04 23:57:05 +0300

    lesson-6 init

--- desktop/src/com/dune/game/desktop/DesktopLauncher.java
+++ desktop/src/com/dune/game/desktop/DesktopLauncher.java
@@ -9,6 +9,7 @@ public class DesktopLauncher {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.width = 1280;
 		config.height = 720;
+//		config.foregroundFPS = 1;
 		new LwjglApplication(new DuneGame(), config);
 	}
 }
